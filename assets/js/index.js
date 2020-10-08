56662d84ca455957438d72068eb347b489658220
yangwenguang <m00120089>
2020-10-08 20:43:53 +0800

    2

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,18 +19,18 @@ $(function() {
 
 // 获取用户信息封装函数
 // 注意，位置写到入口函数外面，后面代码中要使用这个方法但是要求这个方法是一个全局方法
-function getUserInof() {
+function getUserInfo() {
     $.ajax({
         url: '/my/userinfo',
         success: function(res) {
-            console.log(res);
+            // console.log(res);
             // 判断状态码
             if (res.status !== 0) {
                 return layui.layer.msg(res.message);
 
             }
             // 请求成功，渲染用户头像信息
-            renderAvator(res.data)
+            renderAvatar(res.data)
         },
         // 无论成功失败，都是触发complete方法
         //    complete: function(res) {
@@ -52,19 +52,19 @@ function getUserInof() {
 
 
 // 封装用户头像渲染函数
-function renderAvator(user) {
+function renderAvatar(user) {
     // 用户名（昵称优先，没有就用username）
     var name = user.nickname || user.username;
-    $("#welcome").hmtl("欢迎&nbsp;&nbsp" + name);
+    $("#welcome").html("欢迎&nbsp;&nbsp" + name);
     // 用户头像
     if (user.user_pic !== null) {
         // 有头像
         $(".layui-nav-img").show().attr("src", user.user_pic);
-        $(".text-avatar").hide();
+        $(".user-avatar").hide();
     } else {
         // 没有头像
         $(".layui-nav-img").hide();
         var text = name[0].toUpperCase();
-        $(".text-avatar").show().html(text);
+        $(".user-avatar").show().html(text);
     }
 }
